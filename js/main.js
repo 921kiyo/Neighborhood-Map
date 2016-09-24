9161745e34cb925697bc815a82aa09ac62eb07ad
Kiyo <kiyo921@hotmail.co.jp>
2016-09-24 11:43:40 +0100

    Add 10 locations in Edinburgh

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ var map;
 
 function initMap(){
   map = new google.maps.Map(document.getElementById("map"), {
-    // center: {lat: 55.953252, lng:-3.188267},
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: {lat: 55.953252, lng:-3.188267},
     zoom: 13
   });
-  console.log(map);
+  var locations = [
+  {title:"Calton Hill", location{lat: 55.9553471, lng:-3.1825288}},
+  {title:"Edinburgh Castle", location{lat: 55.9485947, lng:-3.1999135}},
+  {title:"Arthur's Seat", location{lat: 55.9440833, lng:-3.1618333}},
+  {title:"Scottish National Gallery of Modern Art", location{lat: 55.95143179999999, lng:-3.2254509}},
+  {title:"National Museum of Scotland", location{lat: 55.94704, lng:-3.191668}},
+  {title:"Grassmarket", location{lat: 55.9475383, lng:-3.1984856}},
+  {title:"Scottish National Gallery", location{lat: 55.9509865, lng:-3.1986358}},
+  {title:"Royal Botanic Garden Edinburgh", location{lat: 55.9512388, lng:-3.2331183}},
+  {title:"Palace of Holyroodhouse", location{lat: 55.9465981, lng:-3.2095417}},
+  {title:"Scott Monument", location{lat: 55.9513812, lng:-3.1986984}},
+  ]
 }
